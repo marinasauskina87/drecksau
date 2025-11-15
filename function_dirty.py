e2ef7a31e4192f7835ddc12ea61ce50835f1c630
Prasovic Amer (prasoame) <prasoame@students.zhaw.ch>
2025-11-15 17:53:31 +0100

    Update function_dirty.py

--- function_dirty.py
+++ function_dirty.py
@@ -3,6 +3,8 @@ def action_dirty(dictionary: dict, pig_number: str) -> dict:
     """
     Die Funktion trägt in einem Wörterbuch ein, dass ein bestimmtes Schwein jetzt als „dirty“ markiert ist.
     """
-    dictionary[f"{pig_number}"] = "dirty"
+    status = dictionary[pig_number]
+    status = status.replace('clean,', 'dirty,')
+    dictionary[pig_number] = status
     return dictionary
 
