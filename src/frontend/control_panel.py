39d3dd82efbc93ae9c29e0fbab185d19529c291e
Lucien <leimbluc@students.zhaw.ch>
2025-11-25 21:18:50 +0100

    Adapt logic to gui

--- src/frontend/control_panel.py
+++ src/frontend/control_panel.py
@@ -1,13 +1,41 @@
 from customtkinter import *
 
-import config
-import manage_pigs
-import manage_action_cards
-import manage_support_cards
+import frontend.config as config
+import frontend.manage_pigs as manage_pigs
+import frontend.manage_action_cards as manage_action_cards
+import frontend.manage_support_cards as manage_support_cards
+
+from backend.action_clean import action_clean
+from backend.function_rain import action_rain
+from backend.create_dictionary import create_dictionary
+from backend.action_stable_open import action_stable_open
+from backend.action_flash import action_flash
+from backend.function_dirty import action_dirty
+from backend.function_stable import action_stable
+from backend.action_stable_locked import action_stable_locked
+from backend.function_current_arrester import action_current_arrester
+from backend.function_rain import action_rain
+from backend.action_tornado import action_tornado
+
+def update_UI(root_window, player_number, pig_number, player_dict):
+    pigs = root_window.nametowidget(f".pigs_player_{int(player_number)}")
+    chosen_pig = pigs.winfo_children()[int(pig_number)-1]
+    states = player_dict[f'pig_{pig_number}']
+    if "dirty" in states:
+        manage_pigs.change_state_pig(root_window, player_number, pig_number, "dirty")
+    elif "clean" in states:
+        manage_pigs.change_state_pig(root_window, player_number, pig_number, "clean")
+    if "stable" in states:
+        manage_support_cards.add_support_card(root_window, player_number, pig_number, "Stallkarte")
+    if "arrester" in states:
+        manage_support_cards.add_support_card(root_window, player_number, pig_number, "Blitzableiterkarte")
+    if "locked" in states:
+        manage_support_cards.add_support_card(root_window, player_number, pig_number, "Bauer-ärgere-dich-Karte")
 
 def parse_widget_to_pig_location(widget):
     """
-    Returns firstly 
+    Helper function to receive the player-number and the pig-number of the selected pigs
+    more gently 
     """
     return_values = {
         "player_number": 0,
@@ -17,23 +45,38 @@ def parse_widget_to_pig_location(widget):
         if "pigs_player" in part_of_name:
             return_values["player_number"] = part_of_name[-1]
         elif "ctkbutton" in part_of_name:
-            if (part_of_name[-1] == 'n'):
-                return_values["pig_number"] = 1
-            else:
-                return_values["pig_number"] = part_of_name[-1]
+            return_values["pig_number"] = widget.flag
     return return_values
 
 def setup_command_pigs(root_window):
-    for player in range(config.amount_of_players):
-        pigs = root_window.nametowidget(f".pigs_player_{player+1}")
+    # During pig-button initialisation, the command-function (executed when clicked) couldn't
+    # be initialised as it is in another module. Therefore, it is done here
+    for player in range(1, config.amount_of_players+1):
+        pigs = root_window.nametowidget(f".pigs_player_{player}")
         for pig in pigs.winfo_children():
+            # As we just want to tackle the buttons and not the label "Player X"
             if (pig.__class__.__name__ == "CTkButton"):
                 pig.configure(command=lambda button=pig: trigger_action(button))
 
 def setup_command_action_card(root_window):
+    # During action-card-button initialisation, the command-function (executed when clicked) couldn't
+    # be initialised as it is in another module. Therefore, it is done here
     action_cards = root_window.nametowidget(f".action_cards")
     for action_card in action_cards.winfo_children():
-        action_card.configure(command=lambda button=action_card: deselect_action_cards(button))
+        # As we just want to tackle the buttons and not the label "It's your turn..."
+        if action_card.__class__.__name__ == "CTkButton":
+            action_card.configure(command=lambda button=action_card: handle_action_card_selection(button))
+
+def handle_played_action_card(current_player, played_card, card_dict_players):
+    # Remove the played card
+    for index, card in enumerate(card_dict_players[f"player-{current_player}"]):
+        if card == played_card:
+            card_dict_players[f"player-{current_player}"].pop(index)
+    
+    # Draw a card
+    new_card = manage_action_cards.choose_random_card()
+    card_dict_players[f"player-{current_player}"].append(new_card[0])
+    return card_dict_players
 
 def trigger_action(clicked_btn):
     # TODO: Change it as label
@@ -42,89 +85,114 @@ def trigger_action(clicked_btn):
     # Get the selected action_card
     action_card_frame = root.nametowidget(f".action_cards")
     for action_card in action_card_frame.winfo_children():
-        if action_card.cget("state") == "enabled":
-            selected_pig = parse_widget_to_pig_location(clicked_btn)
-            selected_pig_number = selected_pig["pig_number"]
-            # For later implementation:
-            print("SELECTED ACTION CARD:")
-            print(action_card)
-            print(action_card.cget("image").pil_image)
-            print(action_card.cget("image").pil_image.split('.')[0])
-            print("---------------------")
-            print("SELECTED PIG:")
-            print(selected_pig_number)
-            print(clicked_btn.winfo_parent())
-            print(clicked_btn)
-            print("-------------------------")
+        # Just select the selected action_card
+        if action_card.__class__.__name__ == "CTkButton" and action_card.flag == "Selected":
+            # Define entities
+            selected_entity = parse_widget_to_pig_location(clicked_btn)
+            selected_pig = selected_entity["pig_number"]
+            selected_player = selected_entity["player_number"]
             selected_action_card = action_card.cget("image").pil_image.split('.')[0]
-            if (selected_action_card in config.status_cards):
-                manage_pigs.change_state_pig(root, int(str(clicked_btn.winfo_parent())[-1]), selected_pig_number, "dirty")
-            elif (selected_action_card in config.actional_cards):
-                ## TODO: Implement logic
-                print("actional")
-            elif (selected_action_card in config.support_cards):
-                ## TODO: Implement logic
-                selected_player = selected_pig["player_number"]
-                manage_support_cards.add_support_card(root, selected_player, selected_pig_number, selected_action_card)
+            selected_player_dict = globals()[f"dict_player_pigs_{selected_player}"]
 
+            handle_played_action_card(current_player, selected_action_card, card_dict_players)
+
+            # If a status card has been played:
+            if (selected_action_card in config.status_cards):
+                selected_player_dict = action_dirty(selected_player_dict, f"pig_{selected_pig}")
+            # If an action card has been played:
+            elif (selected_action_card in config.actional_cards):
+                if (selected_action_card == "Blitzkarte"):
+                    selected_player_dict = action_flash(selected_player_dict, f"pig_{selected_pig}")
+                elif (selected_action_card == "Bauer-schrubbt-die-Sau-Karte"):
+                    selected_player_dict = action_stable_open(selected_player_dict, selected_pig)
+                else:
+                    print(f"Card not found {selected_action_card}")
+            # If a support card has been played:
+            elif (selected_action_card in config.support_cards):
+                if (selected_action_card == "Stallkarte"):
+                    selected_player_dict = action_stable(selected_player_dict, f"pig_{selected_pig}")
+                elif selected_action_card == "Blitzableiterkarte":
+                    selected_player_dict = action_current_arrester(selected_player_dict, f"pig_{selected_pig}")
+                elif selected_action_card == "Bauer-ärgere-dich-Karte":
+                    selected_player_dict = action_stable_locked(selected_player_dict, f"pig_{selected_pig}")
+            
+            # Update the GUI after changes
+            update_UI(root, selected_player, selected_pig, selected_player_dict)
+
+    # Standard configuration after move
     current_player = config.configure_current_player(current_player)
-    enable_player_cards()
+    change_state_player_pigs(root, "disabled")
     manage_action_cards.show_action_cards(root, card_dict_players, current_player)
     setup_command_action_card(root)
 
-    return None
-
-def deselect_action_cards(button):
-    enable_player_cards()
-
-    # Remove the played card
-    name_of_card = button.cget("image").pil_image.split('.')[0]
-    for index, card in enumerate(card_dict_players[f"player-{current_player}"]):
-        if card == name_of_card:
-            card_dict_players[f"player-{current_player}"].pop(index)
-
-    # Draw a card
-    new_card = manage_action_cards.choose_random_card()
-    card_dict_players[f"player-{current_player}"].append(new_card[0])
-
-    # Disable all cards except the selected one
-    frame = root.nametowidget(f".action_cards")
-    for action_card in frame.winfo_children():
-        if button == action_card:
-            action_card.configure(state="enabled")
+def handle_action_card_selection(clicked_action_card):
+    selected_action_card = clicked_action_card.cget("image").pil_image.split('.')[0]
+    # Spread moves don't need a specific pig selection
+    if (selected_action_card == "Regenkarte") or (selected_action_card == "Tornadokarte"):
+        # Get a list of all pig-dictionaries
+        list_of_all_pigs = []
+        for player_dict in range(config.amount_of_players):
+            list_of_all_pigs.append(globals()[f"dict_player_pigs_{player_dict+1}"])
+        
+        if (selected_action_card == "Regenkarte"):
+            list_of_all_pigs = action_rain(list_of_all_pigs)
         else:
-            action_card.configure(state="disabled")
+            list_of_all_pigs = action_tornado(list_of_all_pigs)
+        
+        # Update the GUI for every pig
+        for player in range(1, config.amount_of_players+1):
+            for pig in range(1, config.pigs_per_player[config.amount_of_players]+1):
+                update_UI(root, player, pig, globals()[f"dict_player_pigs_{player}"])
 
-def enable_player_cards():
+        # Standard configuration after move
+        change_state_player_pigs(root, "disabled")
+        manage_action_cards.show_action_cards(root, card_dict_players, config.configure_current_player(current_player))
+        setup_command_action_card(root)
+        if (selected_action_card == "Regenkarte"):
+            handle_played_action_card(current_player, "Regenkarte", card_dict_players)
+        else:
+            handle_played_action_card(current_player, "Tornadokarte", card_dict_players)
+    # If no spread-move has been played
+    else:
+        # The player should only select pigs if he has selected an action-card
+        change_state_player_pigs(root, "enabled")
+        for action_card in clicked_action_card.master.winfo_children():
+            # Clear the selected-flags of previously (potentially) selected action-cards
+            if hasattr(action_card, "flag"):
+                action_card.flag = "Inactive"
+        # Change the new action-card to "selected"
+        clicked_action_card.flag = "Selected"
+
+def change_state_player_pigs(root_window, new_state):
+    # Make the pigs of all players clickable (only available after an action card has been chosen)
     for player in range(config.amount_of_players):
-        frame = root.nametowidget(f".pigs_player_{player+1}")
-        children = frame.winfo_children()
-        for widget in children:
+        frame_player_pigs = root_window.nametowidget(f".pigs_player_{player+1}")
+        for widget in frame_player_pigs.winfo_children():
+            # Don't select the potential support-cards of the pig
             if not widget.__class__.__name__ == "Frame":
-                widget.configure(state="enabled")
+                widget.configure(state=new_state)
 
 # -------------------------------------------------------------------------------
+
+## Basic configuration
 current_player = 1
 card_dict_players = config.configure_player_card_dictionary(config.amount_of_players)
 
 root = config.configure_board()
 
-# TODO: Delete after final implementation
-# Reason: Demonstration purposes 
-btn = CTkButton(root, text="Delete", command=lambda: manage_support_cards.remove_support_card(root, 2, 2, "Stallkarte"))
-btn.grid(row=0,column=0)
-
-# GENERAL TODO: Experiment with the following:
-# btn.flag = "Hallo"
-
-# Pigs
+# Pigs preparation
 root = manage_pigs.add_pigs(root)
 setup_command_pigs(root)
 
+<<<<<<< Updated upstream:src/frontend/control_panel.py
 # Action cards
 for player in range(1, config.amount_of_players + 1):
+=======
+# Action cards preparation
+for player in range(1,config.amount_of_players+1):
+    globals()[f"dict_player_pigs_{player}"] = create_dictionary(config.amount_of_players).copy()
+>>>>>>> Stashed changes:frontend/control_panel.py
     manage_action_cards.draw_cards(player, 3, card_dict_players)
-
 manage_action_cards.create_frame(root)
 manage_action_cards.show_action_cards(root, card_dict_players, current_player)
 setup_command_action_card(root)
--- src/frontend/manage_pigs.py
+++ src/frontend/manage_pigs.py
@@ -1,28 +1,28 @@
-import config
 from customtkinter import *
 from tkinter import Frame
 from PIL import Image, ImageTk, ImageFont, ImageDraw  # Import PIL for PNG support
 
+import frontend.config as config
+
+
 def prepare_pig_image(player, pig_state):
+    positions = config.get_positions(config.amount_of_players)
+
     # Image-Path can be accessed by "img_pig.filename"
-    img_pig = Image.open(f"images/{pig_state}.png")
+    img_pig = Image.open(f"frontend/images/{pig_state}.png")
     rez_img_pig = img_pig.resize((int(img_pig.width*0.8), int(img_pig.height*0.8)))
-
-    rez_img_pig_rot = rez_img_pig.rotate(config.positions[player][3], expand=True)
-
+    rez_img_pig_rot = rez_img_pig.rotate(positions[player][3], expand=True)
     widget_img_pig = ImageTk.PhotoImage(rez_img_pig_rot)
 
     return widget_img_pig
 
 def add_pigs(root_window):
-    pigs_per_player = {
-        2: 5,
-        3: 4,
-        4: 3
-    }
-    amount_of_pigs = pigs_per_player[config.amount_of_players]
+    amount_of_pigs = config.pigs_per_player[config.amount_of_players]
+    positions = config.get_positions(config.amount_of_players)
+    background_color_pigs = "violet"
 
     for player in range(config.amount_of_players):
+<<<<<<< Updated upstream:src/frontend/manage_pigs.py
         group_of_pigs = Frame(root_window, bg="#242424", name=f"pigs_player_{player+1}")
         group_of_pigs.grid(row=config.positions[player][0], column=config.positions[player][1], sticky=
         config.positions[player][2])
@@ -52,24 +52,73 @@ def add_pigs(root_window):
             tk_img = ImageTk.PhotoImage(img)
             lbl = CTkLabel(root_window, image=tk_img, text="")
             lbl.grid(row=2, column=4, sticky="nsw")
+=======
+        group_of_pigs = Frame(root_window, bg=background_color_pigs, name=f"pigs_player_{player+1}")
+        group_of_pigs.grid(row=positions[player][0], column=positions[player][1], sticky=positions[player][2])
+>>>>>>> Stashed changes:frontend/manage_pigs.py
 
         for pig in range(amount_of_pigs):
-            btn = CTkButton(group_of_pigs, text="", image=prepare_pig_image(player, "Sauberschwein"), fg_color="transparent", width=0, state="disabled")
-            if (config.positions[player][3] % 180 == 0):
-                btn.grid(column=pig, row=0)
+            btn_pig = CTkButton(group_of_pigs, text="", image=prepare_pig_image(player, "Sauberschwein"), fg_color="transparent", width=0, state="disabled")
+            # Needed for an faster recognition of the selected pig
+            btn_pig.flag = pig+1
+            if (positions[player][3] % 180 == 0):
+                btn_pig.grid(column=pig, row=0)
             else:
-                btn.grid(row=pig, column=0)
+                btn_pig.grid(row=pig, column=0)
+        
+        # Needed to get the correct and updated widths of the widgets
+        root_window.update()
+
+        font_lbl_player = ImageFont.truetype("arial.ttf", 40)
+        ## Horizontal image
+        img_lbl_player_hor = Image.new("RGBA", (group_of_pigs.winfo_width(), 50), background_color_pigs)
+        draw_lbl_player_hor = ImageDraw.Draw(img_lbl_player_hor)
+        # Calculation from ChatGPT (based on pixel-width, amount of characters in string and scaling due to fontsize)
+        draw_lbl_player_hor.text(((img_lbl_player_hor.width-(0.4 * 40 * len("Player X")))/2, 0), f"Player {player+1}", fill="white", font=font_lbl_player)
+        
+        ## Vertical image
+        img_lbl_player_ver = Image.new("RGBA", (group_of_pigs.winfo_height(), 50), background_color_pigs)
+        draw_lbl_player_ver = ImageDraw.Draw(img_lbl_player_ver)
+        # Calculation from ChatGPT (based on pixel-width, amount of characters in string and scaling due to fontsize)
+        draw_lbl_player_ver.text(((img_lbl_player_ver.width-(0.4 * 40 * len("Player X")))/2, 0), f"Player {player+1}", fill="white", font=font_lbl_player)
+
+        # Due to the differentiation of vertical and horizontal and the different sticky-attributes than the
+        # config-file, we can't simplify the code below
+        if positions[player][2] == "s":
+            tk_img_lbl_player = ImageTk.PhotoImage(img_lbl_player_hor)
+            lbl_player = CTkLabel(root_window, image=tk_img_lbl_player, text="", fg_color=background_color_pigs)
+            lbl_player.grid(row=4, column=2, sticky="n")
+        elif positions[player][2] == "n":
+            tk_img_lbl_player = ImageTk.PhotoImage(img_lbl_player_hor)
+            lbl_player = CTkLabel(root_window, image=tk_img_lbl_player, text="", fg_color=background_color_pigs)
+            lbl_player.grid(row=0, column=2, sticky="s")
+        elif positions[player][2] == "w":
+            # Rotate it due to verticality
+            img_lbl_player_ver = img_lbl_player_ver.rotate(270, expand=True)
+            tk_img_lbl_player = ImageTk.PhotoImage(img_lbl_player_ver)
+            lbl_player = CTkLabel(root_window, image=tk_img_lbl_player, text="")
+            lbl_player.grid(row=2, column=0, sticky="e")
+        else:
+            # Rotate it due to verticality
+            img_lbl_player_ver = img_lbl_player_ver.rotate(90, expand=True)
+            tk_img_lbl_player = ImageTk.PhotoImage(img_lbl_player_ver)
+            lbl_player = CTkLabel(root_window, image=tk_img_lbl_player, text="", anchor="center")
+            lbl_player.grid(row=2, column=4, sticky="w")
         
     return root_window
 
 def change_state_pig(root_window, player, pig, new_state):
+    player = int(player)
     if new_state == "dirty":
         new_img_widget = prepare_pig_image(player-1, "Dreckssau")
-    else:
+    elif new_state == "clean":
         new_img_widget = prepare_pig_image(player-1, "Sauberschwein")
+    else:
+        return f"Wrong state: {new_state}"
     
     frame = root_window.nametowidget(f"pigs_player_{player}")
 
+    # Change image of selected file (-1 due to zero-indexing)
     widget = frame.winfo_children()[int(pig)-1]
     widget.configure(image = new_img_widget)
 
