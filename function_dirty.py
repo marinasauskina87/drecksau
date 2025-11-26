cee21d3f35134526c23eb073147ca8dad6be788b
lauras-star <milicla1@students.zhaw.ch>
2025-11-26 12:55:31 +0100

    kommas beim status entfernt

--- function_dirty.py
+++ function_dirty.py
@@ -4,7 +4,7 @@ def action_dirty(dictionary: dict, pig_number: str) -> dict:
     Die Funktion trägt in einem Wörterbuch ein, dass ein bestimmtes Schwein jetzt als „dirty“ markiert ist.
     """
     status = dictionary[pig_number]
-    status = status.replace('clean,', 'dirty,')
+    status = status.replace('clean', 'dirty')
     dictionary[pig_number] = status
     return dictionary
 
