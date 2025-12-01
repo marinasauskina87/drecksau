b1daab520c92569c75eeef5afc2f549b6f13f779
Sauskina Marina (samar001) <samar001@students.zhaw.ch>
2025-12-01 14:00:12 +0100

    clean Funktion angepast.

--- src/backend/action_clean.py
+++ src/backend/action_clean.py
@@ -4,7 +4,9 @@ def action_clean(dictionary: dict, pig_number: str) -> dict:
     """
     Die Funktion trägt in einem Wörterbuch ein, dass ein bestimmtes Schwein jetzt als „clean“ markiert ist.
     """
-    dictionary[f"{pig_number}"] = 'clean'
+    status = dictionary[f"{pig_number}"]
+    status = status.replace("dirty", "clean")
+    dictionary[f"{pig_number}"] = status
     return dictionary
 
 
